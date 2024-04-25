0bb3b1480764c4fcc4200ffe1576232e9add2600
Peter Diepstraten <psrdiepstraten@gmail.com>
2024-04-25 13:53:44 +0200

    Setup Project

---
+++ springlink/src/main/java/dev/peter/springlink/SpringLinkApplication.java
@@ -0,0 +1,13 @@
+package dev.peter.springlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLinkApplication.class, args);
+	}
+
+}
